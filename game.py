69ed33aa2e4c783dad9b66fc55a18a2c94bcde82
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-10-25 20:51:23 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,19 @@
+# This program adds two numbers provided by the user
+
+# Get input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Add the two numbers
+sum = num1 + num2
+
+# Display the result
+print("The sum of {0} and {1} is {2}".format(num1, num2, sum))
+
+'''
+O/P
+Enter first number: 1
+Enter second number: 2
+The sum of 1.0 and 2.0 is 3.0
+
+'''
